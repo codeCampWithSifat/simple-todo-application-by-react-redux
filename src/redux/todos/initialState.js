973d76fe5e2505a40029695f5ee90b8d7845a0fa
Sayed <sayedhossainsifat300@gmail.com>
2022-08-31 00:14:58 +0600

    full done

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+const initialState = [
+    {
+        id : 1,
+        text : "learn React js",
+        completed : true
+    },
+    {
+        id : 2,
+        text :"Learn Node Js From Learn With Sumit",
+        color : 'red',
+        completed : false ,
+    }
+];
+
+export default initialState;
